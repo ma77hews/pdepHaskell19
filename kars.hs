d08133f1651befaf38a7b731ecf3e5c7cf01f8d0
napster <napster@localhost.localdomain>
2019-04-18 11:25:25 -0300

    subo modelado de los autos

--- kars.hs
+++ kars.hs
@@ -2,6 +2,32 @@ type Truco = Auto
 data Auto = Auto {  nombre :: String,
                     nivelDeNafta :: Int,
                     velocidad :: Int,
-                    nombreDelEnamorado :: String,
-                    truco :: Truco
-                    }deriving Show
+                    nombreDelEnamorado :: String
+                    --truco :: Truco
+                    } deriving Show
+
+rochaMcQueen = Auto { nombre = "rochaMcQueen",
+                      nivelDeNafta = 300,
+                      velocidad = 0,
+                      nombreDelEnamorado = "ronco"
+                      --truco = deReversa
+                    }
+
+biankerr = Auto { nombre ="biankerr",
+                  nivelDeNafta = 500,
+                  velocidad = 0,
+                  nombreDelEnamorado = "tinch"
+                  --truco = impresionar
+                  }
+gushtav = Auto { nombre = "gushtav",
+                 nivelDeNafta = 200,
+                 velocidad = 130,
+                 nombreDelEnamorado = "petiLaLinta"
+                 --truco = nitro
+                }
+rodra = Auto {  nombre = "rodra",
+                nivelDeNafta = 0,
+                velocidad = 50,
+                nombreDelEnamorado = "taisa"
+                --truco = fingirAmor(petra)
+                }
